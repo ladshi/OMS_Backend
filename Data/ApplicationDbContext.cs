c45eaf62f1657aa52d507636e4fefd47e6bc78a1
Lakshiga Mahintherrajah <lakshiga20021216@gmail.com>
2026-01-08 09:38:58 +0530

    Auth flow full finish

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using OMS_Backend.Models;
+
+namespace OMS_Backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(e => e.Email).IsUnique();
+        });
+    }
+}
