5a8be1fd4ebf6e89ee0dd0e77c0a1ef7de1f16eb
ladshi <shikaladhu@gmail.com>
2026-01-09 09:12:49 +0530

    latest change

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using OMS_Backend.Entities;
 
-namespace OMS_Backend.Data
-{
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+namespace OMS_Backend.Data;
 
-        public DbSet<Customer> Customers { get; set; } = null!;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<Product> Product { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(e => e.Email).IsUnique();
+        });
     }
 }
--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,6 +1,32 @@
-﻿namespace OMS_Backend.Entities
+using OMS_Backend.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OMS_Backend.Entities
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string ProductName { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Required]
+        public ProductStatus ProductStatus { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
     }
 }
